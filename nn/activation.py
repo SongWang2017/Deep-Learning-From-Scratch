392d7ab23955e270dd055ad831c55488f5a9de24
SongWang2017 <wang.song@columbia.edu>
2019-05-02 23:10:10 +0800

    activation function defined

---
+++ nn/activation.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def step_function(x):
+    y = x > 0
+    return y.astype(np.int)
+
+x = np.array([1.0, 2.0, -1.0])
+
+print(step_function(x))
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+x = np.array([-1, 1, 0])
+print(sigmoid(x))
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x, y)
+plt.ylim(0, 1)
+plt.show()
+
+def relu(x):
+    return np.maximum(0, x)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = relu(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 5.1)
+plt.show()
