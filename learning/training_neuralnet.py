b29f6d6d44b258c4725169393a908b16271b32b9
SongWang2017 <wang.song@columbia.edu>
2019-05-08 22:19:09 +0800

    logging the traing and testing error for every epoch

--- learning/training_neuralnet.py
+++ learning/training_neuralnet.py
@@ -6,9 +6,14 @@ import matplotlib.pyplot as plt
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize = True, one_hot_label = True)
 
 train_loss_list = []
+train_acc_list = []
+test_acc_list = []
 
-iters_num = 100
+
+#hyperparameters
+iters_num = 10000
 train_size = x_train.shape[0]
+iter_per_epoch = np.max(train_size / batch_size, 1)
 batch_size = 100
 learning_rate = 0.1
 
@@ -33,6 +38,16 @@ for i in range(iters_num):
     #logging the learning process
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
+
+    #logging the accuracy every batch
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+        
 
 plt.figure(figsize=(16, 6))
 plt.plot(train_loss_list)
